94123555035df888d7cd094e6f7f445a3dcbdf84
Matt Heerspink <mheerspink75@gmail.com>
2021-10-24 02:22:13 -0500

    first commit

--- src/lib.js
+++ src/lib.js
@@ -1,25 +1,36 @@
-import fs from 'fs'
+import fs from "fs";
 
-export const chooseRandom = () => {
+export const chooseRandom = (array, number) => {
   // TODO implement chooseRandom
-}
+  if (array.length === 0 || array.length === 1) {
+    return array;
+  }
+  if (array.length > 1) {
+    let arr = [];
+    for (let i = 0; i < number; i++) {
+      let numItems = array[i];
+      arr.push(numItems);
+    }
+    return arr;
+  }
+};
 
 export const createPrompt = () => {
   // TODO implement createPrompt
-}
+};
 
 export const createQuestions = () => {
   // TODO implement createQuestions
-}
+};
 
-export const readFile = path =>
+export const readFile = (path) =>
   new Promise((resolve, reject) => {
-    fs.readFile(path, (err, data) => (err ? reject(err) : resolve(data)))
-  })
+    fs.readFile(path, (err, data) => (err ? reject(err) : resolve(data)));
+  });
 
 export const writeFile = (path, data) =>
   new Promise((resolve, reject) => {
-    fs.writeFile(path, data, err =>
-      err ? reject(err) : resolve('File saved successfully')
-    )
-  })
+    fs.writeFile(path, data, (err) =>
+      err ? reject(err) : resolve("File saved successfully")
+    );
+  });
